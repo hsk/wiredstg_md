8b2a518d38caefc01fa1799b2c99da449b7f1917
Hiroshi Sakurai <sakurai777@gmail.com>
2024-09-15 14:54:09 +0900

    14

--- src/Title.c
+++ src/Title.c
@@ -18,22 +18,37 @@ void TitleInitialize(void) {
     VDP_setHorizontalScroll(BG_A,0);
     updateSprite = 1;
 }
+static void updateAnim(void) {
+    // アニメーションの更新
+    if (titleAnimation<0x60) {
+        titleAnimation++;
+        u8 x = titleAnimation&31;
+        u8 y = titleAnimation/32;
+        if (4 <= x && x < 4+24 && y < 4) {
+            VDP_setTileMapXY(BG_A,TILE_USER_INDEX+title_map[x-4+y*24],x+4,y+7);
+        }
+    }
+}
+static void drawSpace(void) {
+    // ハイスコアの描画
+    if (titleAnimation<0x60)return;
+
+    if (titleTimer&0x10) VDP_drawText("PRESS START BUTTON",7+4,18);
+    else                 VDP_drawText("                  ",7+4,18);
+}
 // タイトルを更新する
 void TitleUpdate(void) {
     if (titleState==0) {// 初期化の開始
         titleAnimation = 0; // アニメーションの初期化
         titleTimer = 0;     // タイマの初期化
         titleState++;       // 初期化の完了
-        VDP_setTileMapDataRectEx(BG_A, title_map,TILE_USER_INDEX, 8, 7, 24, 3, 24, CPU);
-
-//        VDP_setTileMapDataRectEx(BG_A,TILE_USER_INDEX,4,7);
-        VDP_drawText("PRESS START BUTTON",7+4,18);
     }
     random();// 乱数の更新
     if (titleState==1) {// START キーの監視
         titleTimer++;// タイマの更新
         if ((input & BUTTON_START) || (input & BUTTON_A)) {// START キーの押下
             titleState++;
+            while(titleAnimation<0x60)updateAnim();
         }
     } else {
         titleTimer += 8;// タイマの更新
@@ -41,4 +56,6 @@ void TitleUpdate(void) {
             appState = APP_STATE_GAME_INITIALIZE;// ゲームの開始
         }
     }
+    updateAnim();
+    drawSpace();
 }
